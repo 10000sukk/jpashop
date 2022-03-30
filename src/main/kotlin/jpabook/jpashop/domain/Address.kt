3f8b3a9062b9216dab05eda3fc0c13c5f249e693
echo(김환석) <echo.ohce@kakaocorp.com>
2022-03-31 01:11:28 +0900

    value object entity 수정

--- src/main/kotlin/jpabook/jpashop/domain/Address.kt
+++ src/main/kotlin/jpabook/jpashop/domain/Address.kt
@@ -3,15 +3,8 @@ package jpabook.jpashop.domain
 import javax.persistence.*
 
 @Embeddable
-open class Address(
-    city:String? = null,
-    street:String? = null,
-    zipcode:String? = null) {
-
-//    @Id @GeneratedValue
-//    @Column(name = "address_id")
-//    open var id:Long? = null
-    open var city:String? = city
-    open var street:String? = street
-    open var zipcode:String? = zipcode
-}
+data class Address(
+    private var city: String? = null,
+    private var street: String? = null,
+    private var zipcode: String? = null
+)
